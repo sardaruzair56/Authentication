75787dfc6a9b254fb7d1372a93444d82cab4d988
sardaruzair56 <116259618+sardaruzair56@users.noreply.github.com>
2023-10-26 12:02:32 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-import EditEventPage from './pages/EditEvent';
-import ErrorPage from './pages/Error';
+import EditEventPage from "./pages/EditEvent";
+import ErrorPage from "./pages/Error";
 import EventDetailPage, {
   loader as eventDetailLoader,
   action as deleteEventAction,
-} from './pages/EventDetail';
-import EventsPage, { loader as eventsLoader } from './pages/Events';
-import EventsRootLayout from './pages/EventsRoot';
-import HomePage from './pages/Home';
-import NewEventPage from './pages/NewEvent';
-import RootLayout from './pages/Root';
-import { action as manipulateEventAction } from './components/EventForm';
-import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
+} from "./pages/EventDetail";
+import EventsPage, { loader as eventsLoader } from "./pages/Events";
+import EventsRootLayout from "./pages/EventsRoot";
+import HomePage from "./pages/Home";
+import NewEventPage from "./pages/NewEvent";
+import RootLayout from "./pages/Root";
+import { action as manipulateEventAction } from "./components/EventForm";
+import NewsletterPage, { action as newsletterAction } from "./pages/Newsletter";
+import AuthenticationPage, {
+  action as authAction,
+} from "./pages/Authentication";
+import { logoutAction } from "./pages/logout";
+import { checkAuthLoader, tokenLoader } from "./util/auth";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <RootLayout />,
+    id:'root',
+    loader:tokenLoader,
     errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
       {
-        path: 'events',
+        path: "events",
         element: <EventsRootLayout />,
         children: [
           {
@@ -31,8 +38,8 @@ const router = createBrowserRouter([
             loader: eventsLoader,
           },
           {
-            path: ':eventId',
-            id: 'event-detail',
+            path: ":eventId",
+            id: "event-detail",
             loader: eventDetailLoader,
             children: [
               {
@@ -40,25 +47,41 @@ const router = createBrowserRouter([
                 element: <EventDetailPage />,
                 action: deleteEventAction,
               },
+
               {
-                path: 'edit',
+                path: "edit",
                 element: <EditEventPage />,
                 action: manipulateEventAction,
+                loader:checkAuthLoader
               },
             ],
           },
+
           {
-            path: 'new',
+            path: "new",
             element: <NewEventPage />,
             action: manipulateEventAction,
+            loader:checkAuthLoader
           },
         ],
       },
+      
       {
-        path: 'newsletter',
+        path: "auth",
+        element: <AuthenticationPage />,
+        action: authAction,
+      },
+      {
+        path: "newsletter",
         element: <NewsletterPage />,
         action: newsletterAction,
       },
+
+      {
+        path:"logout",
+        action:logoutAction,
+      },
+      
     ],
   },
 ]);
--- src/components/MainNavigation.js
+++ src/components/MainNavigation.js
@@ -1,13 +1,27 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, Form, useRouteLoaderData } from "react-router-dom";
 
-import classes from './MainNavigation.module.css';
-import NewsletterSignup from './NewsletterSignup';
+import classes from "./MainNavigation.module.css";
+import NewsletterSignup from "./NewsletterSignup";
 
 function MainNavigation() {
+
+ const token=useRouteLoaderData('root');
+
   return (
     <header className={classes.header}>
       <nav>
         <ul className={classes.list}>
+          {!token && <li>
+            <NavLink
+              to="auth?mode=login"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+              end
+            >
+              Authentication
+            </NavLink>
+          </li>}
           <li>
             <NavLink
               to="/"
@@ -39,6 +53,11 @@ function MainNavigation() {
               Newsletter
             </NavLink>
           </li>
+          {token &&<li>
+            <Form action="logout" method="post">
+              <button>Logout</button>
+            </Form>
+          </li>}
         </ul>
       </nav>
       <NewsletterSignup />
